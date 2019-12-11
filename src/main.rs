62584ddbf1636c768d8d73bb40ff91314249e1cf
snOm3ad <geronimooliver00@gmail.com>
2019-12-11 14:07:21 -0500

    created instruction wrapper
    advent of code was pissing me off with that many requirements for the
    intcode machine, that I just decided to take it seriously and make a
    fully functional wrapper for machine instructions that come in the form
    of integers. this one is neat because it only allocates a small buffer
    upon creation but then after that updates itself without having to
    allocate memory.

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ use solutions::day_01;
 use solutions::day_02;
 use solutions::day_03;
 use solutions::day_04;
+use solutions::day_05;
 
 fn main() {
     day_01::run(day_01::Part::All);
     day_02::run(day_02::Part::All);
     day_03::run(day_03::Part::All);
     day_04::run(day_04::Part::All);
+    day_05::run(day_05::Part::One);
 }
--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,4 @@
+use digits_iterator::*;
 use std::error::Error;
 use std::fs::File;
 use std::io;
@@ -20,4 +21,63 @@ where
         v.push(content);
     }
     Ok(v)
+}
+
+pub mod wrapper {
+    use super::*;
+
+    #[derive(Default, Debug)]
+    pub struct Instruction {
+        value: isize,
+        digits: Vec<u8>,
+        pub opcode: [u8; 5],
+    }
+
+    impl Instruction {
+        pub fn new(value: isize) -> Self {
+            let mut ins = Self::default();
+            ins.update(value);
+            ins
+        }
+
+        pub fn get_value(&self) -> isize {
+            self.value
+        }
+
+        pub fn update(&mut self, value: isize) {
+            // if the capacity is 0, then the call comes from new, which means we need to allocate
+            // memory to store the digit representation of value.
+            if self.digits.capacity() == 0 {
+                // allocate memory and never bother again.
+                self.digits.reserve(16);
+            }
+            // now update the digits and upcode.
+            self.value = value;
+            self.update_digits();
+            self.update_opcode();
+        }
+
+        fn update_digits(&mut self) {
+            // using the iterator avoids allocating memory for a new vector which is the desired
+            // behavior. NOTE that digits will transform the isize to the respective unsigned
+            // representation.
+            for digit in self.value.digits() {
+                self.digits.push(digit);
+            }
+        }
+
+        fn update_opcode(&mut self) {
+            // NOTE: before we update the opcode we need to override the current value of the
+            // opcode, as we might be updating from a value with a greater number of digits to one
+            // with less digits. This means that if the previous opcode was `[0, 0, 1, 2, 3]` and
+            // we are updating to the value `5` then we want opcode to be `[0, 0, 0, 0, 5]` as
+            // opposed of `[0, 0, 1, 2, 5]`.
+            self.opcode = [0; 5];
+            // now update the upcode.
+            let opcode = self.digits.iter().rev().take(5);
+            for (i, digit) in opcode.enumerate() {
+                self.opcode[4 - i] = *digit;
+            }
+        }
+    }
 }
